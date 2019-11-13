ebc12346bf1b939bfcdd74587f24e643bb8d5334
fernando313 <53427743+fernando313@users.noreply.github.com>
2019-11-13 16:57:22 +0300

    Add files via upload

---
+++ advogrand/js/core.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+    $('.feedback-slider-item.active').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button class="prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow: '<button class="next"><i class="fa fa-angle-right"></i></button>',
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 3000
+                }
+            }
+        ]
+    });
+
+
+    // табы 
+
+    $('.js-tab-trigger').on('click', function () {
+        let tabName = $(this).data('tab');
+        let tab = $('.js-tabs-content[data-tab="' + tabName + '"]');
+
+
+
+        $('.js-tab-trigger.active').removeClass('active');
+        $(this).addClass('active');
+
+        $('.js-tabs-content.active').removeClass('active');
+        tab.addClass('active');
+        $('.js-tabs-content.active').slick({
+            infinite: false,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: '<button class="prev"><i class="fa fa-angle-left"></i></button>',
+            nextArrow: '<button class="next"><i class="fa fa-angle-right"></i></button>',
+            responsive: [{
+                    breakpoint: 992,
+                    settings: {
+                        arrows: false,
+                        slidesToShow: 2,
+                        autoplay: true,
+                        autoplaySpeed: 2000
+
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    });
+    // готовый слайдер с партнерами
+    $('.partners').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow: '<button class="next"><i class="fa fa-angle-right"></i></button>',
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 3,
+                    autoplay: true,
+                    autoplaySpeed: 4000
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 4000
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 4000
+                }
+            }
+        ]
+    });
+
+    // модальные окна
+
+
+    $('.recall-btn').on('click', function (e) {
+        console.log(this);
+        e.preventDefault();
+        $('.recall-modal').fadeIn();
+    });
+    $('.recall-close').click(function (e) {
+        e.preventDefault();
+        $('.recall-modal').fadeOut();
+    });
+});
